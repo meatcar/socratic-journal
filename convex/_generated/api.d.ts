1409e2a7fc9a4d326aee1b982568258b620b87cc
Denys Pavlov <me@denys.me>
2025-07-25 13:01:38 -0400

    feat: consolidate openai types

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -16,6 +16,7 @@ import type {
 import type * as auth from "../auth.js";
 import type * as http from "../http.js";
 import type * as journal from "../journal.js";
+import type * as lib_openai from "../lib/openai.js";
 import type * as router from "../router.js";
 
 /**
@@ -30,6 +31,7 @@ declare const fullApi: ApiFromModules<{
   auth: typeof auth;
   http: typeof http;
   journal: typeof journal;
+  "lib/openai": typeof lib_openai;
   router: typeof router;
 }>;
 export declare const api: FilterApi<
